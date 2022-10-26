147883caeed3a1b69d9a8599ed093badf64baf96
Alesyahaponenko <alesyahaponenko@gmail.com>
2022-10-26 16:20:41 +0300

    add form

--- src/components/MobileVer.js
+++ src/components/MobileVer.js
@@ -1,11 +1,11 @@
-import React, {Suspense, useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState} from "react";
 import {gsap} from "gsap";
 import PreloadImage from "../services/PreloadImage";
-// import GoButton from "./GoButton";
-// import BuyForm from "./BuyForm";
-// import SearchBar from "./searchBar";
-// import arrow_next from "../images/lang/arrowDown.svg";
-// import arrow_mob from "../images/lang/arrow_mob.svg";
+import GoButton from "./GoButton";
+import BuyForm from "./BuyForm";
+import SearchBar from "./searchBar";
+import arrow_next from "../images/lang/arrowDown.svg";
+import arrow_mob from "../images/lang/arrow_mob.svg";
 
 import path from "../images/mobile/mob/path.png"
 import player from "../images/mobile/mob/player.png"
@@ -16,7 +16,7 @@ import corner from "../images/mobile/mob/corner.png"
 import tablo from "../images/mobile/mob/tablo.png"
 import vorota from "../images/mobile/mob/vorota.png"
 import zritel from "../images/mobile/mob/zritel.png"
-// import france from "../images/lang/france.png"
+import france from "../images/lang/france.png"
 
 
 function IntroMobile() {
@@ -171,9 +171,9 @@ function IntroMobile() {
                 .fromTo(q(".zritel"),
                     {xPercent: 30, yPercent: 30},
                     {xPercent: 0, yPercent: 0, duration: 1, ease: "power3.inOut"}, "<-=0.5")
-                // .fromTo(q(".goButton"),
-                //     {yPercent: 200},
-                //     {yPercent: 0, duration: 1, ease: "power3.inOut"}, "<")
+                .fromTo(q(".goButton"),
+                    {yPercent: 200, opacity:0},
+                    {yPercent: 0, opacity:1, duration: 1, ease: "power3.inOut"}, "<")
                 .to('.appears', {opacity: 1, duration: 1})
                 .fromTo(q('.path'),
                     {opacity: 0},
@@ -185,92 +185,92 @@ function IntroMobile() {
             ctx_l.revert();
         }
     }, [])
-    //
-    //
-    // const startForm = () => {
-    //     setStart(true)
-    //     tl_start.current = gsap.timeline({paused: true})
-    //         .set(q(".player"), {zIndex: 10})
-    //         .set(q(".zritel"), {zIndex: 0})
-    //         .set(".buy_wrap", {top: 0, zIndex: 9})
-    //         .to(".buy_form_wrap", {opacity: 1, duration: 1, ease: "power4.inOut"})
-    //         .to(q(".middle_box"), {top: '0vh', duration: 2, ease: "power4.inOut"}, "<")
-    //         .to(q(".player"), {top: '20vh', left: -20, duration: 2, ease: "power4.inOut"}, "<")
-    //         .to('.chat_wrap', {top: '70vh', height: '15vh', duration: 2, ease: "power4.inOut"}, "<")
-    //
-    //     tl_start.current.play()
-    // }
-    // const back_to_main = () => {
-    //     tl_start.current.reverse()
-    //
-    // }
-    // useEffect(() => {
-    //     tl_Ref.current = gsap.timeline({paused: true})
-    //     tl_Ref.current.to(".chat_wrap", {autoAlpha: 0, ease: 'power3.inOut'})
-    //     tl_Ref.current.to(".social_buttons_wrap", {autoAlpha: 0, ease: 'power3.inOut'})
-    //     tl_Ref.current.to(q(".lang_panel"), {autoAlpha: 1, bottom: '0', ease: 'power3.inOut'})
-    // }, [tl_Ref, q])
-    //
-    // useEffect(() => {
-    //     tl_langPanel_Ref.current = gsap.timeline({paused: true})
-    //     tl_langPanel_Ref.current.to(q(".go_title"), {opacity: 0, y: 50, ease: 'none'})
-    // }, [tl_langPanel_Ref, q])
-    //
-    // const langClick = () => {
-    //     if (!langRef.current.classList.contains('active')) {
-    //         langRef.current.classList.add('active')
-    //         tl_Ref.current.play()
-    //     } else {
-    //         langRef.current.classList.remove('active')
-    //         tl_Ref.current.reverse()
-    //     }
-    // }
-    // const subLangClick = () => {
-    //     langRef.current.classList.remove('active')
-    //     tl_Ref.current.reverse()
-    // }
-    //
-    // const openLangPanel = () => {
-    //     if (!langFullPanelRef.current.classList.contains("active")) {
-    //         langFullPanelRef.current.classList.add("active")
-    //         gsap.timeline().to(q(".lang_full_panel"), {autoAlpha: 1, duration: 0.3})
-    //             .to(q(".panel_l"), {marginTop: 0, opacity: 1, duration: 1, ease: "back"}, "<")
-    //             .fromTo(q(".liLang"), {opacity: 0}, {opacity: 1, duration: 1, ease: "back"}, "<")
-    //     } else {
-    //         langFullPanelRef.current.classList.remove("active")
-    //     }
-    // }
-    //
-    // const closeLangPanel = () => {
-    //     if (langFullPanelRef.current.classList.contains("active")) {
-    //         gsap.timeline().to(q(".panel_l"), {marginTop: 100, duration: 1, ease: "back"})
-    //             .to(q(".lang_full_panel"), {autoAlpha: 0, duration: 1.2}, "<")
-    //         langFullPanelRef.current.classList.remove("active")
-    //         langRef.current.classList.remove('active')
-    //         tl_Ref.current.reverse()
-    //     }
-    // }
-    //
-    // const wrapClosePanel = (e) => {
-    //     if (e.target.classList.contains("lang_full_wrap")) {
-    //         gsap.timeline().to(q(".panel_l"), {marginTop: 100, duration: 1, ease: "back"})
-    //             .to(q(".lang_full_panel"), {autoAlpha: 0, duration: 1.2}, "<")
-    //         langFullPanelRef.current.classList.remove("active")
-    //     }
-    // }
-    //
-    // const langs = [
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france},
-    //     {lang: 'fran', img: france}
-    // ]
+
+
+    const startForm = () => {
+        setStart(true)
+        tl_start.current = gsap.timeline({paused: true})
+            .set(q(".player"), {zIndex: 10})
+            .set(q(".zritel"), {zIndex: 0})
+            .set(".buy_wrap", {top: 0, zIndex: 9})
+            .to(".buy_form_wrap", {opacity: 1, duration: 1, ease: "power4.inOut"})
+            .to(q(".middle_box"), {top: '0vh', duration: 2, ease: "power4.inOut"}, "<")
+            .to(q(".player"), {top: '20vh', left: -20, duration: 2, ease: "power4.inOut"}, "<")
+            .to('.chat_wrap', {top: '70vh', height: '15vh', duration: 2, ease: "power4.inOut"}, "<")
+
+        tl_start.current.play()
+    }
+    const back_to_main = () => {
+        tl_start.current.reverse()
+
+    }
+    useEffect(() => {
+        tl_Ref.current = gsap.timeline({paused: true})
+        tl_Ref.current.to(".chat_wrap", {autoAlpha: 0, ease: 'power3.inOut'})
+        tl_Ref.current.to(".social_buttons_wrap", {autoAlpha: 0, ease: 'power3.inOut'})
+        tl_Ref.current.to(q(".lang_panel"), {autoAlpha: 1, bottom: '0', ease: 'power3.inOut'})
+    }, [tl_Ref, q])
+
+    useEffect(() => {
+        tl_langPanel_Ref.current = gsap.timeline({paused: true})
+        tl_langPanel_Ref.current.to(q(".go_title"), {opacity: 0, y: 50, ease: 'none'})
+    }, [tl_langPanel_Ref, q])
+
+    const langClick = () => {
+        if (!langRef.current.classList.contains('active')) {
+            langRef.current.classList.add('active')
+            tl_Ref.current.play()
+        } else {
+            langRef.current.classList.remove('active')
+            tl_Ref.current.reverse()
+        }
+    }
+    const subLangClick = () => {
+        langRef.current.classList.remove('active')
+        tl_Ref.current.reverse()
+    }
+
+    const openLangPanel = () => {
+        if (!langFullPanelRef.current.classList.contains("active")) {
+            langFullPanelRef.current.classList.add("active")
+            gsap.timeline().to(q(".lang_full_panel"), {autoAlpha: 1, duration: 0.3})
+                .to(q(".panel_l"), {marginTop: 0, opacity: 1, duration: 1, ease: "back"}, "<")
+                .fromTo(q(".liLang"), {opacity: 0}, {opacity: 1, duration: 1, ease: "back"}, "<")
+        } else {
+            langFullPanelRef.current.classList.remove("active")
+        }
+    }
+
+    const closeLangPanel = () => {
+        if (langFullPanelRef.current.classList.contains("active")) {
+            gsap.timeline().to(q(".panel_l"), {marginTop: 100, duration: 1, ease: "back"})
+                .to(q(".lang_full_panel"), {autoAlpha: 0, duration: 1.2}, "<")
+            langFullPanelRef.current.classList.remove("active")
+            langRef.current.classList.remove('active')
+            tl_Ref.current.reverse()
+        }
+    }
+
+    const wrapClosePanel = (e) => {
+        if (e.target.classList.contains("lang_full_wrap")) {
+            gsap.timeline().to(q(".panel_l"), {marginTop: 100, duration: 1, ease: "back"})
+                .to(q(".lang_full_panel"), {autoAlpha: 0, duration: 1.2}, "<")
+            langFullPanelRef.current.classList.remove("active")
+        }
+    }
+
+    const langs = [
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france},
+        {lang: 'fran', img: france}
+    ]
 
     return (
         <div className="intro_wrap" ref={containerRef}>
@@ -318,64 +318,64 @@ function IntroMobile() {
                 <div className="player img"><PreloadImage alt="" src={player}/></div>
                 <div className="zritel img"><PreloadImage alt="" src={zritel}/></div>
                 <div className="ball img"><PreloadImage alt="" src={ball}/></div>
-                {/*<div className="goButton">*/}
-                {/*    <GoButton startForm={startForm} start={start} langClick={langClick}/>*/}
-                {/*</div>*/}
+                <div className="goButton">
+                    <GoButton startForm={startForm} start={start} langClick={langClick}/>
+                </div>
 
-                {/*<div className="lang_panel" ref={langRef}>*/}
-                {/*    <ul>*/}
-                {/*        {*/}
-                {/*            langs.slice(0, 5).map((el, index) => (*/}
-                {/*                    <li key={index} onClick={subLangClick} className="liLang">*/}
-                {/*                        <div className="name">{el.lang}</div>*/}
-                {/*                        <div className="flag">*/}
-                {/*                            <img alt="" src={el.img}/>*/}
-                {/*                        </div>*/}
-                {/*                    </li>*/}
-                {/*                )*/}
-                {/*            )*/}
-                {/*        }*/}
-                {/*    </ul>*/}
-                {/*    <div className="arrow_mob" onClick={openLangPanel}>*/}
-                {/*        <SuspenseImg alt="" src={arrow_mob}/>*/}
-                {/*    </div>*/}
-                {/*    <div className="next_arrow" onClick={openLangPanel}>*/}
-                {/*        <SuspenseImg alt="" src={arrow_next}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="lang_panel" ref={langRef}>
+                    <ul>
+                        {
+                            langs.slice(0, 5).map((el, index) => (
+                                    <li key={index} onClick={subLangClick} className="liLang">
+                                        <div className="name">{el.lang}</div>
+                                        <div className="flag">
+                                            <img alt="" src={el.img}/>
+                                        </div>
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+                    <div className="arrow_mob" onClick={openLangPanel}>
+                        <PreloadImage alt="" src={arrow_mob}/>
+                    </div>
+                    <div className="next_arrow" onClick={openLangPanel}>
+                        <PreloadImage alt="" src={arrow_next}/>
+                    </div>
+                </div>
 
-                {/*<div className="lang_full_panel" ref={langFullPanelRef}>*/}
-                {/*    <div className="lang_full_wrap" onClick={wrapClosePanel}>*/}
-                {/*        <div className="panel_l">*/}
-                {/*            <div className="panel_close_btn" onClick={closeLangPanel}>*/}
-                {/*                <span></span>*/}
-                {/*                <span></span>*/}
-                {/*            </div>*/}
-                {/*            <div className="search_pos">*/}
-                {/*                <SearchBar/>*/}
-                {/*            </div>*/}
-                {/*            <div className="allLangs">*/}
-                {/*                <ul>*/}
-                {/*                    {*/}
-                {/*                        langs.map((el, index) => (*/}
-                {/*                                <li key={index} className="liLang">*/}
-                {/*                                    <div className="name">{el.lang}</div>*/}
-                {/*                                    <div className="flag">*/}
-                {/*                                        <img alt="" src={el.img}/>*/}
-                {/*                                    </div>*/}
-                {/*                                </li>*/}
-                {/*                            )*/}
-                {/*                        )*/}
-                {/*                    }*/}
-                {/*                </ul>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="lang_full_panel" ref={langFullPanelRef}>
+                    <div className="lang_full_wrap" onClick={wrapClosePanel}>
+                        <div className="panel_l">
+                            <div className="panel_close_btn" onClick={closeLangPanel}>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div className="search_pos">
+                                <SearchBar/>
+                            </div>
+                            <div className="allLangs">
+                                <ul>
+                                    {
+                                        langs.map((el, index) => (
+                                                <li key={index} className="liLang">
+                                                    <div className="name">{el.lang}</div>
+                                                    <div className="flag">
+                                                        <img alt="" src={el.img}/>
+                                                    </div>
+                                                </li>
+                                            )
+                                        )
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                {/*<div className="buy_wrap">*/}
-                {/*    <BuyForm back_to_main={back_to_main}/>*/}
-                {/*</div>*/}
+                <div className="buy_wrap">
+                    <BuyForm back_to_main={back_to_main}/>
+                </div>
 
             </div>
         </div>
